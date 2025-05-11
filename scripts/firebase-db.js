c1baabb1b77ed470d4a3b01751528bde64eb5817
Dusnoki Stefan <sdusnoki@constructor.university>
2025-05-11 20:05:46 +0200

    Referencing UID when making course

--- scripts/firebase-db.js
+++ scripts/firebase-db.js
@@ -1,5 +1,23 @@
-import { db } from "./firebase-init.js";
+import { auth, db } from "./firebase-init.js";
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+
+let currentUID = null;
+
+onAuthStateChanged(auth, (user) => {
+    try{
+        if (user) {
+        currentUID = user.uid;
+        console.log("User authenticated:", currentUserUID);
+        } else {
+            console.warn("No authenticated user. Redirecting to login.");
+            window.location.href = "../auth_pages/login_page.html";
+        }
+    } catch(e) {
+        console.log("ERROR: " + e);
+    }
+    
+});
 
 async function createCourse(courseName, objective, desiredLength, learningStyle, userGoals, curriculum) {
     try {
@@ -9,7 +27,8 @@ async function createCourse(courseName, objective, desiredLength, learningStyle,
             desired_length: desiredLength,
             learning_style: learningStyle,
             user_goals: userGoals,
-            curriculum: curriculum
+            curriculum: curriculum,
+            created_by_user: currentUID
         });
         console.log("Course created! ID: ", course.id);
         window.location.href = "../../acc_pages/user_dashboard.html"
